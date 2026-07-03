5874f119f073866a075d3ffc52b76dea44152043
nicolasBoth <nicolasboth07@gmail.com>
2026-07-03 19:08:12 -0300

    fix: adjusting depoiments section responsitivity

--- src/components/Depoiments/index.tsx
+++ src/components/Depoiments/index.tsx
@@ -5,17 +5,19 @@ export default function Depoiments(props: DepoimentsProps) {
   return (
     <>
       <div className="avaliacao">
-        <div className="avaliacao-secao1">
-          <img src={props.foto} />
-        </div>
-        <div className="avaliacao-secao2">
-          <h3>{props.nome}</h3>
-          <div className="estrelas">
-            <FaStar size={32} color="rgb(255, 59, 59)" />
-            <FaStar size={32} color="rgb(255, 59, 59)" />
-            <FaStar size={32} color="rgb(255, 59, 59)" />
-            <FaStar size={32} color="rgb(255, 59, 59)" />
-            <FaStar size={32} color="rgb(255, 59, 59)" />
+        <div className='avaliacao-container'>
+          <div className="avaliacao-secao1">
+            <img src={props.foto} />
+          </div>
+          <div className="avaliacao-secao2">
+            <h3>{props.nome}</h3>
+            <div className="estrelas">
+              <FaStar size={32} color="rgb(255, 59, 59)" />
+              <FaStar size={32} color="rgb(255, 59, 59)" />
+              <FaStar size={32} color="rgb(255, 59, 59)" />
+              <FaStar size={32} color="rgb(255, 59, 59)" />
+              <FaStar size={32} color="rgb(255, 59, 59)" />
+            </div>
           </div>
         </div>
         <article>{props.texto}</article>
